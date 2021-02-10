1bde44b07d6b8a70db75c7a7f47ba54d199982bc
hsadoqi <hsadoqi@gmail.com>
2021-02-09 23:51:21 -0500

    async redux lecture code

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,24 @@
 class ListsController < ApplicationController
     def index 
-        render json: ListSerializer.new(List.all)
+        render json: List.all
     end 
 
     def create 
         list = List.create(list_params)
-        render json: ListSerializer.new(list)
+        render json: list
     end
+
+    def update 
+        list = List.find_by(id: params[:id])
+        list.update(list_params)
+        render json: list
+    end 
+
+    def destroy 
+        list = List.find_by(id: params[:id])
+        list.destroy 
+        render json: {message: "success"}
+    end 
 
     private 
 
